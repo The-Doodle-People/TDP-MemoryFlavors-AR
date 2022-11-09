2ecd937431eef47f09c1481514a5903e3d372f02
jasmine-g <92961208+jasmine-g@users.noreply.github.com>
2022-11-10 01:49:58 +0800

    add in script to detect touch
    not working but no errors

---
+++ MemoryFlavours_AR/Assets/Scripts/TouchHandler.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class TouchHandler : MonoBehaviour
+{
+    void OnTouchPress()
+    {
+        
+        //x n y coordinated of the touch
+        Vector3 rayPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+
+        //nearclipplane is where u start seeing things
+        rayPosition.z = Camera.main.nearClipPlane;
+
+        Ray ray = Camera.main.ScreenPointToRay(rayPosition);
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            if (hitInfo.collider.tag == "dry mix")
+            {
+                Debug.Log("touched");
+            }
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
