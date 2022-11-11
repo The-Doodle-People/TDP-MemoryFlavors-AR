c8cf4c757f5153dbf39a9522b0b4c9970cba6484
Guo Ji Xuan <S10222055@connect.np.edu.sg>
2022-11-11 19:09:43 +0800

    Start Screen Set up. Loading Screen Implemented
    Loading screen is just an animation

--- MemoryFlavours_AR/Assets/0_TDPFolderTemplate/Scripts/UIController.cs
+++ MemoryFlavours_AR/Assets/0_TDPFolderTemplate/Scripts/UIController.cs
@@ -6,6 +6,47 @@ using Vuforia;
 public class UIController : MonoBehaviour
 {
     public GameObject restartConfirmation;
+    public Animator loadingScreen;
+    public GameObject mainUI;
+    public GameObject startCanvas;
+    bool experienceStarted;
+
+    void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (/*Input.touchCount > 0*/ Input.GetMouseButtonDown(0) && !experienceStarted)
+        {
+            StartCoroutine("PlayLoading");
+        }
+    }
+
+    IEnumerator PlayLoading()
+    {
+        experienceStarted = true;
+        loadingScreen.SetBool("experienceStart", true);
+        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(4.5f);
+        ToggleMainUI();
+        loadingScreen.SetBool("experienceStart", false);
+        startCanvas.SetActive(false);
+    }
+
+    public void ToggleMainUI()
+    {
+        if (!mainUI.activeSelf)
+        {
+            mainUI.SetActive(true);
+        }
+        
+        else if (mainUI.activeSelf)
+        {
+            mainUI.SetActive(false);
+        }
+    }
 
     public void ToggleCamera()
     {
