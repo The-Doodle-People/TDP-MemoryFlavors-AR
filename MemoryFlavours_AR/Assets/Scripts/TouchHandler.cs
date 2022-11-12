cbf7a835b638af9849bff0e5befdee10381790c8
Ng shijun Jess <S10222791@connect.np.edu.sg>
2022-11-12 13:33:36 +0800

    adding touch Handler

---
+++ MemoryFlavours_AR/Assets/Scripts/TouchHandler.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchHandler : MonoBehaviour
+{
+    void OnTouchPress()
+    {
+        Debug.Log("Touch Detected");
+    }
+}
