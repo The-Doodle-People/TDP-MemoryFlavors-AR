57b63f042d9180b890191c744ccd0d2878012b98
chlshng <s10223325@connect.np.edu.sg>
2022-11-15 18:45:52 +0800

    Player movement
    Infinite repeated tiles and collision of bomb has been added.

---
+++ MemoryFlavours_AR/Assets/Scripts/TileManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    public GameObject[] tilePrefabs;
+    public float zSpawn = 0;
+    public float tileLength = 1;
+    public int numberofTiles = 5;
+    private List<GameObject> activeTiles = new List<GameObject>();
+
+    public Transform playerTransform;
+    void Start()
+    {
+        for(int i= 0; i < tilePrefabs.Length; i++)
+        {
+            if (i == 0)
+                SpawnTile(0);
+            else
+                SpawnTile(Random.Range(0, tilePrefabs.Length));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerTransform.position.z - 35 >zSpawn-(numberofTiles*tileLength))
+        {
+            SpawnTile(Random.Range(0, tilePrefabs.Length));
+            DeleteTile();
+        }
+    }
+    public void SpawnTile(int tileIndex)
+    {
+        GameObject go = Instantiate(tilePrefabs[tileIndex],transform.forward*zSpawn,transform.rotation);
+        activeTiles.Add(go);
+        zSpawn += tileLength;
+    }
+    private void DeleteTile()
+    {
+        Destroy(activeTiles[0]);
+        activeTiles.RemoveAt(0);
+    }
+}
