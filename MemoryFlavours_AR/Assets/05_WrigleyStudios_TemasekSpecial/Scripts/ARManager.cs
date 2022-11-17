3aefa57c585f134baf3ba2afeb7a82da82a46f34
Melvyn <S10228085@connect.np.edu.sg>
2022-11-17 21:12:20 +0800

    Added Template Quiz and Question
    Added the quiz section where the question and the answer can be placed later .

--- MemoryFlavours_AR/Assets/05_WrigleyStudios_TemasekSpecial/Scripts/ARManager.cs
+++ MemoryFlavours_AR/Assets/05_WrigleyStudios_TemasekSpecial/Scripts/ARManager.cs
@@ -2,23 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class ARManager : MonoBehaviour
 {
     public TextMeshProUGUI displayText;
-    public TextMeshProUGUI quizQuestion;
-
-    public GameObject audioBtn;
-    public GameObject quizBtn;
-    public GameObject shareBtn;
-    public GameObject clearBtn;
-    public GameObject helpBtn;
-    public GameObject whiteNoiseBtn;
-
+    public bool isTracked = false;
     public GameObject mainCanvas;
-    public GameObject quizCanvas;
-
-    public bool checkQuiz = false;
 
     /// <summary>
     /// This dictionary holds the tracked status of AR objects
@@ -28,6 +18,12 @@ public class ARManager : MonoBehaviour
     void Awake()
     {
         mainCanvas.SetActive(true);
+        displayText.text = "";
+    }
+
+    private void Update()
+    {
+        //LoadQuizScene();
     }
 
     public void ObjectTracked(GameObject objectToTrack)
@@ -35,60 +31,72 @@ public class ARManager : MonoBehaviour
         
         if (objectToTrack != null)
         {
+            isTracked = true;
+            Debug.Log(isTracked);
             objectsTracked[objectToTrack] = true;
             //displayText.text = objectToTrack.name + " Track Status: true";
             Debug.Log(objectToTrack.name);
             if (objectToTrack.name == "DevilCurry")
             {
+                StaticController.objectName = objectToTrack.name;
                 displayText.text = objectToTrack.name + "<insert Description here>";
-                OpenQuiz(objectToTrack.name);
+                
             }
             else if (objectToTrack.name == "NasiPandang")
             {
+                StaticController.objectName = objectToTrack.name;
                 displayText.text = objectToTrack.name + " <insert Description here>";
-                OpenQuiz(objectToTrack.name);
+                
             }
             else if (objectToTrack.name == "HokkienMee")
             {
+                StaticController.objectName = objectToTrack.name;
                 displayText.text = objectToTrack.name + "In Singapore, Hokkien mee (???) refers to a dish of" +
                     " egg noodles and rice noodles stir-fried with egg, slices of pork, prawns, and squid. The key" +
                     " to the dish is copious quantities of an aromatic broth made from prawns and pork bones, slowly" +
                     " simmered for many hours. Sambal chili and calamansi limes are served on the side for the diner" +
                     " to blend in, giving it an extra zing and tanginess. Traditionally, small cubes of fried lard are added," +
                     " and some stalls also serve the open on an opeh leaf (soft areca palm bark), which enhances the fragrance of the dish.";
-                OpenQuiz(objectToTrack.name);
+                
             }
             else if (objectToTrack.name == "Thosai")
             {
+                StaticController.objectName = objectToTrack.name;
                 displayText.text = objectToTrack.name + "Thosai (also spelled dosa) is a savory pancake " +
                     "served with a slew of spicy dipping sauces. Like so much of the Indian food popular at" +
                     " Singapore hawker centers, thosai are cheap, tasty and 100% vegetarian.";
-                OpenQuiz(objectToTrack.name);
+                
             }
             else if (objectToTrack.name == "Hawker")
             {
+                StaticController.objectName = objectToTrack.name;
                 displayText.text = objectToTrack.name + " is Hawker Tracked";
-                OpenQuiz(objectToTrack.name);
+                
             }
             else if (objectToTrack.name == "PushCart")
             {
+                StaticController.objectName = objectToTrack.name;
                 displayText.text = objectToTrack.name + " is Push Cart Tracked";
-                OpenQuiz(objectToTrack.name);
+                
             }
+            
         }
         
         else
         {
+            isTracked = false;
             return;
         }
     }
 
     public void ObjectLost(GameObject objectToTrack)
     {
+        Debug.Log("Object lost");
         if (objectToTrack != null)
         {
+            isTracked = false;
             objectsTracked[objectToTrack] = false;
-            //displayText.text = objectToTrack.name + "Track Status: false";
+            displayText.text = "";
         }
         else
         {
@@ -96,15 +104,19 @@ public class ARManager : MonoBehaviour
         }
     }
 
-    public void CheckQuiz()
+    public void LoadQuizScene()
     {
-        checkQuiz = true;
-        Debug.Log(checkQuiz);
-    }
-
-    public void OpenQuiz(string scannedItem)
-    {
-        
+        Debug.Log(isTracked);
+        if (isTracked == true)
+        {
+            
+            SceneManager.LoadScene(1);
+            isTracked = false;
+        }
+        else
+        {
+            return;
+        }
     }
 
 }
--- MemoryFlavours_AR/Assets/05_WrigleyStudios_TemasekSpecial/Scripts/QuizManager.cs
+++ MemoryFlavours_AR/Assets/05_WrigleyStudios_TemasekSpecial/Scripts/QuizManager.cs
@@ -1,18 +1,241 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class QuizManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public TextMeshProUGUI quizQuestionText;
+    public TextMeshProUGUI tryAgainText;
+    public TextMeshProUGUI answerOneText;
+    public TextMeshProUGUI answerTwoText;
+    public TextMeshProUGUI answerThreeText;
+    string objectName;
+    int answer = 0;
+
+
+    private void Update()
     {
         
+
+        Question();
+        MCQ();
+        Debug.Log("Answer: " + answer);
+        Debug.Log("The Food: " + objectName);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        objectName = StaticController.objectName;
+    }
+
+    private void Question()
+    {
+        if (objectName == "DevilCurry")
+        {
+            StaticController.objectName = "";
+            quizQuestionText.text = "Question about Devil Curry";
+            answerOneText.text = "1. <insert answer here>";
+            answerTwoText.text = "2. <insert answer here>";
+            answerThreeText.text = "3. <insert answer here>";
+        }
+        else if (objectName == "NasiPandang")
+        {
+            StaticController.objectName = "";
+            quizQuestionText.text = "Question about Nasi Padang";
+            answerOneText.text = "1. <insert answer here>";
+            answerTwoText.text = "2. <insert answer here>";
+            answerThreeText.text = "3. <insert answer here>";
+
+        }
+        else if (objectName == "HokkienMee")
+        {
+            StaticController.objectName = "";
+            quizQuestionText.text = "Question about Hokkien Mee";
+            answerOneText.text = "1. <insert answer here>";
+            answerTwoText.text = "2. <insert answer here>";
+            answerThreeText.text = "3. <insert answer here>";
+
+        }
+        else if (objectName == "Thosai")
+        {
+            StaticController.objectName = "";
+            quizQuestionText.text = "Question about Thosai";
+            answerOneText.text = "1. <insert answer here>";
+            answerTwoText.text = "2. <insert answer here>";
+            answerThreeText.text = "3. <insert answer here>";
+        }
+        else if (objectName == "Hawker")
+        {
+            StaticController.objectName = "";
+            quizQuestionText.text = "Question about Hawker";
+            answerOneText.text = "1. <insert answer here>";
+            answerTwoText.text = "2. <insert answer here>";
+            answerThreeText.text = "3. <insert answer here>";
+        }
+        else if (objectName == "PushCart")
+        {
+            StaticController.objectName = "";
+            quizQuestionText.text = "Question about Hawker";
+            answerOneText.text = "1. <insert answer here>";
+            answerTwoText.text = "2. <insert answer here>";
+            answerThreeText.text = "3. <insert answer here>";
+        }
+    }
+
+    private void MCQ()
+    {
+        if (objectName == "DevilCurry")
+        {
+            if (answer == 1)
+            {
+                
+                tryAgainText.text = "correct";
+                answer = 0;
+            }
+            else if (answer == 2)
+            {
+               
+                tryAgainText.text = "wrong";
+                answer = 0;
+            }
+            else if (answer == 3)
+            {
+                
+                tryAgainText.text = "wrong";
+                answer = 0;
+            }
+        }
+        else if (objectName == "NasiPandang")
+        {
+            if (answer == 1)
+            {
+                
+                tryAgainText.text = "correct";
+                answer = 0;
+            }
+            else if (answer == 2)
+            {
+                
+                tryAgainText.text = "wrong";
+                answer = 0;
+            }
+            else if (answer == 3)
+            {
+               
+                tryAgainText.text = "wrong";
+                answer = 0;
+            }
+
+        }
+        else if (objectName == "HokkienMee")
+        {
+            if (answer == 1)
+            {
+                
+                tryAgainText.text = "correct";
+                answer = 0;
+            }
+            else if (answer == 2)
+            {
+                
+                tryAgainText.text = "wrong";
+                answer = 0;
+            }
+            else if (answer == 3)
+            {
+                
+                tryAgainText.text = "wrong";
+                answer = 0;
+            }
+
+        }
+        else if (objectName == "Thosai")
+        {
+            if (answer == 1)
+            {
+                
+                tryAgainText.text = "correct";
+                answer = 0;
+            }
+            else if (answer == 2)
+            {
+                
+                tryAgainText.text = "wrong";
+                answer = 0;
+            }
+            else if (answer == 3)
+            {
+               
+                tryAgainText.text = "wrong";
+                answer = 0;
+            }
+
+        }
+        else if (objectName == "Hawker")
+        {
+            if (answer == 1)
+            {
+                
+                tryAgainText.text = "correct";
+                answer = 0;
+            }
+            else if (answer == 2)
+            {
+                
+                tryAgainText.text = "wrong";
+                answer = 0;
+            }
+            else if (answer == 3)
+            {
+                
+                tryAgainText.text = "wrong";
+                answer = 0;
+            }
+
+        }
+        else if (objectName == "PushCart")
+        {
+            if (answer == 1)
+            {
+                
+                tryAgainText.text = "correct";
+                answer = 0;
+            }
+            else if (answer == 2)
+            {
+                
+                tryAgainText.text = "wrong";
+                answer = 0;
+            }
+            else if (answer == 3)
+            {
+                
+                tryAgainText.text = "wrong";
+                answer = 0;
+            }
+
+        }
+    }
+
+    public void AnswerOne()
+    {
+        answer = 1;
+    }
+    public void AnswerTwo()
+    {
+        answer = 2;
+    }
+    public void AnswerThree()
+    {
+        answer = 3;
+    }
+
+    public void GoBackToMain()
+    {
+        SceneManager.LoadScene(0);
     }
 }
