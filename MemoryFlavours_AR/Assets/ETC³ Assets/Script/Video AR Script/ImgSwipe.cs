278a9b9df7572acafb87cd24bfe6a176c3a35830
ThineZ <gunpolt39@gmail.com>
2022-11-18 20:55:02 +0800

    -- Gallery Pic Added -- Modified Gallery Logic

--- MemoryFlavours_AR/Assets/ETC³ Assets/Script/Video AR Script/ImgSwipe.cs
+++ MemoryFlavours_AR/Assets/ETC³ Assets/Script/Video AR Script/ImgSwipe.cs
@@ -10,26 +10,30 @@ public class ImgSwipe : MonoBehaviour
     [Header("Image Set")]
     public Image one;
     public Image two;
+    public Image three;
+    public Image four;
+    public Image five;
+    public Image six;
+    public Image seven;
+    public Image eight;
 
     [Header("Next Button")]
     public Button next;
 
-    [Header("Audio Play Button")]
-    public Button audioBTN;
+    [Header("Previous Button")]
+    public Button previous; 
 
-    [Header("Audio Source")]
-    public AudioSource oneAudio;    
-    public AudioSource twoAudio;
 
     private void Awake()
     {
-        Button nextBTM = next.GetComponent<Button>();
+        Button nextBTN = next.GetComponent<Button>();
 
-        nextBTM.onClick.AddListener(imagerotate);
+        nextBTN.onClick.AddListener(imagerotate);
 
-        Button play = audioBTN.GetComponent<Button>();
+        Button previousBTN = previous.GetComponent<Button>();
 
-        play.onClick.AddListener(audioPlayer);
+        previousBTN.onClick.AddListener(previousimage);
+
     }
 
     private void imagerotate()
@@ -39,21 +43,74 @@ public class ImgSwipe : MonoBehaviour
             two.fillAmount = 1;
             one.fillAmount = 0;
         }
+        else if (two.fillAmount == 1)
+        {
+            three.fillAmount = 1;
+            two.fillAmount = 0;
+        }
+        else if (three.fillAmount == 1)
+        {
+            four.fillAmount = 1;
+            three.fillAmount = 0;
+        }
+        else if (four.fillAmount == 1)
+        {
+            five.fillAmount = 1;
+            four.fillAmount = 0;
+        }
+        else if (five.fillAmount == 1)
+        {
+            six.fillAmount = 1;
+            five.fillAmount = 0;
+        }
+        else if (six.fillAmount == 1)
+        {
+            seven.fillAmount = 1;
+            six.fillAmount = 0;
+        }
+        else if (seven.fillAmount == 1)
+        {
+            eight.fillAmount = 1;
+            seven.fillAmount = 0;
+        }
     }
 
-
-    private void audioPlayer()
+    private void previousimage()
     {
-        if (one.fillAmount == 1 || two.fillAmount == 0)
+        if (eight.fillAmount == 1)
         {
-            oneAudio.Play();
-            twoAudio.Stop();
+            seven.fillAmount = 1;
+            eight.fillAmount = 0;
         }
-
-        if (two.fillAmount == 1 || one.fillAmount == 0)
+        else if (seven.fillAmount == 1)
         {
-            oneAudio.Stop();
-            twoAudio.Play();
+            six.fillAmount = 1;
+            seven.fillAmount = 0;
+        }
+        else if (six.fillAmount == 1)
+        {
+            five.fillAmount = 1;
+            six.fillAmount = 0;
+        }
+        else if (five.fillAmount == 1)
+        {
+            four.fillAmount = 1;
+            five.fillAmount = 0;
+        }
+        else if (four.fillAmount == 1)
+        {
+            three.fillAmount = 1;
+            four.fillAmount = 0;
+        }
+        else if (three.fillAmount == 1)
+        {
+            two.fillAmount = 1;
+            three.fillAmount = 0;
+        }
+        else if (two.fillAmount == 1)
+        {
+            one.fillAmount = 1;
+            two.fillAmount = 0;
         }
     }
 }
