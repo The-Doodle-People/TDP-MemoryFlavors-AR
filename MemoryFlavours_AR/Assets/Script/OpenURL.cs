fbf9e7d8b4e74269f3a2a2cf407e08c48df54313
Brandontmh <s10222989@connect.np.edu.sg>
2022-11-20 05:16:40 +0800

    done

--- MemoryFlavours_AR/Assets/Script/OpenURL.cs
+++ MemoryFlavours_AR/Assets/Script/OpenURL.cs
@@ -15,4 +15,14 @@ public class OpenURL : MonoBehaviour
     {
         
     }
+
+    public void Instagram()
+    {
+        Application.OpenURL("https://www.instagram.com/musttryfood1432/");
+    }
+
+    public void Facebook()
+    {
+        Application.OpenURL("https://www.facebook.com/profile.php?id=100087950293911");
+    }
 }
