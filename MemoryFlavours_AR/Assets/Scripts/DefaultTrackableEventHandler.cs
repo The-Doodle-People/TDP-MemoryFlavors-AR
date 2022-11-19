804bfe089759e11325cb0e8758fbc99904ec1f94
nurul-iffah <s10204076@connect.np.edu.sg>
2022-11-19 13:09:14 +0800

    Update DefaultTrackableEventHandler.cs

--- MemoryFlavours_AR/Assets/Scripts/DefaultTrackableEventHandler.cs
+++ MemoryFlavours_AR/Assets/Scripts/DefaultTrackableEventHandler.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 using Vuforia;
 using UnityEngine.Video;
 
-public class DefaultTrackableEventHandler : MonoBehaviour;
+public class DefaultTrackableEventHandler : MonoBehaviour
 {
     public GameObject videoPlayBack;
 
     // Start is called before the first frame update
     void Start()
     {
-        videoPlayBack = GameObject.Find("videoplayback").GetComponent<VideoPlayer>().Play();
+        //videoPlayBack = GameObject.Find("videoplayback").GetComponent<VideoPlayer>().Play();
         //videoPlayBack.GetComponent<DefaultTrackableEventHandler>().Invoke(videoPlayBack, float);
     }
 
